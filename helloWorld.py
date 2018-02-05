41667de3a5e8aca6ee997b9775cdaee06273ea0e
Zizob <aurelien.febvre@gmail.com>
2018-02-05 17:06:08 +0100

    test

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
-# Exemple de fichier .py sur lequel je vais tester les commandes de base de git, et que je vais héberger sur github
+# Exemple de fichier .py sur lequel je vais tester les commandes de base de
+# git, et que je vais héberger sur github
 def toto():
     print("titi")
 
